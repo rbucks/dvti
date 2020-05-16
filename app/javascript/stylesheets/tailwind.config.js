8ef9164531483db18b915388c51a85983510eee9
Ryan Buckley <rbucks@gmail.com>
2020-05-15 21:22:32 -0700

    works...??

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/ui')({
+      layout: 'sidebar',
+    })
+  ]
+}
