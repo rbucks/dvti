3e161cd01c42fd7f7a2c92be79ad2e6fca5f2c6a
Ryan Buckley <rbucks@gmail.com>
2020-05-19 21:23:29 -0700

    meta stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def app_name
+    "Diablo Valley Tech Initiative"
+  end
+
+  def page_title(title)
+    title.present? ? "#{title}" : "Promoting a thriving tech sector in the Diablo Valley | DVTI "
+  end
+
+  def meta_description(meta_description)
+    meta_description.present? ? "#{meta_description}" : "Studying and promoting the initiatives that enable the Diablo Valley to cultivate and benefit from a thriving tech sector"
+  end
+
+  def flash_class(level)
+    case level
+      when "notice" then "alert alert-info"
+      when "success" then "alert alert-success"
+      when "error" then "alert alert-danger"
+      when "alert" then "alert alert-danger"
+    end
+  end
 end
