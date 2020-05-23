65d6e5aa47af9278a85321f2d75bee26adfef5ed
Ryan Buckley <rbucks@gmail.com>
2020-05-22 21:18:37 -0700

    more pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root 'home#index'
-  get 'home/index'
+
+  devise_for :users
+
+  get :index, to: 'home#index'
+  get :contact, to: 'home#contact'
+  get :about, to: 'home#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
