d95db9e6d032a31e88dc3ea7c50ec5739eaa4510
Ryan Buckley <rbucks@gmail.com>
2020-05-30 14:26:08 -0700

    more pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,24 @@ class HomeController < ApplicationController
   def about
   end
 
+  def move_here
+  end
+
+  def find_job
+  end
+
+  def take_classes
+  end
+
+  def relocate_here
+  end
+
+  def hire_here
+  end
+
+  def network
+  end
+
   def members
   end
 
